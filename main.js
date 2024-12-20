33bdfe7a13a14d2c74defd2c6cbd430713b0c97e
orn-rany <ranyorn@gmail.com>
2024-12-20 06:56:00 -0800

    commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let searchBox = document.querySelector('.search-box');
+let searchIcon = document.querySelector('#search-icon');
+
+searchIcon.onclick = () => {
+    searchBox.classList.toggle('active'); // Toggle the 'active' class
+};
