6c262503bb853b70220d38cce75878113df57746
soknem <povsoknem@gmail.com>
2024-12-20 20:37:22 +0700

    add serch

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let searchBox = document.querySelector('.search-box');
-let searchIcon = document.querySelector('#search-icon');
+let searchBox = document.querySelector(".search-box");
+let searchIcon = document.querySelector("#search-icon");
 
 searchIcon.onclick = () => {
-    searchBox.classList.toggle('active'); // Toggle the 'active' class
+  searchBox.classList.toggle("active");
 };
